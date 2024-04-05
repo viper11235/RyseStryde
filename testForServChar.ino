27546e3a2aa87500e269d225a81c7263ca02fc45
viper11235 <104604385+viper11235@users.noreply.github.com>
2024-04-04 23:33:29 -0400

    Add files via upload

--- testForServChar.ino
+++ testForServChar.ino
@@ -8,26 +8,27 @@ const uint8_t UUID16_SVC[] = {0x23, 0xD1, 0xBC, 0xEA, 0x5F, 0x78, 0x23, 0x15, 0x
 // Characteristic UUID
 const uint8_t UUID16_CHR[] = {0x23, 0xD1, 0xBC, 0xEA, 0x5F, 0x78, 0x23, 0x15, 0xDE, 0xEF, 0x12, 0x12, 0x24, 0x16, 0x00, 0x00};
 
-BLEService service(UUID16_SVC);
-BLECharacteristic characteristic(UUID16_CHR, BLERead, sizeof(uint8_t));
+BLEService ryse_service(UUID16_SVC);
+BLECharacteristic ryse_characteristic(UUID16_CHR, BLERead | BLENotify, sizeof(double));
 
 BLEDis bledis;    // DIS (Device Information Service) helper class instance
 BLEBas blebas;    // BAS (Battery Service) helper class instance
 
-int dataToSend = 25;
+// const int dataToSend = 25;
 
 void setup() {
   // Start bluetooth 
   Bluefruit.begin();
-  characteristic.begin();
+  ryse_service.begin();
 
   // Configure characteristic
-  characteristic.setProperties(CHR_PROPS_READ);
-  characteristic.setPermission(SECMODE_OPEN, SECMODE_OPEN);
-  characteristic.begin();
+  ryse_characteristic.setProperties(CHR_PROPS_READ | CHR_PROPS_NOTIFY);
+  ryse_characteristic.setPermission(SECMODE_OPEN, SECMODE_OPEN);
+  ryse_characteristic.setFixedLen(sizeof(double));
+  ryse_characteristic.begin();
 
   // Start Bluetooth advertising
-  Bluefruit.Advertising.addService(service);
+  Bluefruit.Advertising.addService(ryse_service);
   Bluefruit.Advertising.start(0); // 0 means it will advertise forever
   // Secondary Scan Response packet (optional)
   Bluefruit.ScanResponse.addName();
@@ -41,10 +42,13 @@ void setup() {
 }
 
 void loop() {
-  characteristic.write(&dataToSend, sizeof(dataToSend));
+  double dataToSend = 25;
+  ryse_characteristic.notify(&dataToSend, sizeof(dataToSend));
   
   Serial.print("Sent: ");
   Serial.println(dataToSend);
   
   delay(1000); // 1 sec delay
 }
+
+// specify names 
